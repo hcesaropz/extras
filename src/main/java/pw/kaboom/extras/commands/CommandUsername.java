9ce1ffb6e71ffe78c7b482f005957afc5b27f1db
hcesaropz <61612754+hcesaropz@users.noreply.github.com>
2026-08-19 06:36:58 +0800

    feat: add /username auto|default|reset

--- src/main/java/pw/kaboom/extras/commands/CommandUsername.java
+++ src/main/java/pw/kaboom/extras/commands/CommandUsername.java
@@ -3,6 +3,8 @@ package pw.kaboom.extras.commands;
 import com.destroystokyo.paper.profile.PlayerProfile;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.MessageComponentSerializer;
@@ -12,10 +14,12 @@ import org.bukkit.entity.Player;
 import pw.kaboom.extras.util.Utility;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static com.mojang.brigadier.arguments.StringArgumentType.greedyString;
 import static io.papermc.paper.command.brigadier.Commands.argument;
+import static io.papermc.paper.command.brigadier.Commands.literal;
 
 public final class CommandUsername implements BrigadierCommand {
     private static final SimpleCommandExceptionType EX_NOT_PLAYER =
@@ -26,8 +30,10 @@ public final class CommandUsername implements BrigadierCommand {
                     Component.text("Please wait a few seconds before changing your username.")));
     private static final SimpleCommandExceptionType EX_TAKEN =
             new SimpleCommandExceptionType(MessageComponentSerializer.message().serialize(
-                    Component.text("Please wait a few seconds before changing your username.")));
+                    Component.text("A player with that username is already logged in.")));
+    private static final long RATELIMIT_MILLIS = 2000;
     private final Map<Player, Long> lastUsedMillis = new HashMap<>();
+    private final Map<Player, String> originalNames = new HashMap<>();
 
     @Override
     public String getLabel() {
@@ -41,58 +47,88 @@ public final class CommandUsername implements BrigadierCommand {
 
     @Override
     public void build(LiteralArgumentBuilder<CommandSourceStack> builder) {
-        builder
-                .requires(src ->
-                        src.getSender().hasPermission("extras.username")
-                                && src.getSender() instanceof Player
-                )
-                // TODO: perhaps "off" lit here to match /skin
-                .then(argument("username", greedyString())
-                        .executes(ctx -> {
-                            if (!(ctx.getSource().getSender() instanceof Player player)) {
-                                throw EX_NOT_PLAYER.create();
-                            }
+        builder.requires(src ->
+                src.getSender().hasPermission("extras.username")
+                        && src.getSender() instanceof Player
+        );
 
-                            String nameColor =
-                                    Utility.translateLegacyColors(
-                                            StringArgumentType.getString(
-                                                    ctx,
-                                                    "username"
-                                            )
-                                    );
-                            String name = nameColor.substring(0, Math.min(16,
-                                    nameColor.length()));
-                            long millis = lastUsedMillis.getOrDefault(player, 0L);
-                            long millisDifference = System.currentTimeMillis() - millis;
+        for (String alias : List.of("auto", "default", "reset")) {
+            builder.then(literal(alias).executes(ctx -> {
+                Player player = player(ctx);
+                String original = originalNames.get(player);
 
-                            if (name.equals(player.getName())) {
-                                player.sendMessage(Component
-                                        .text("You already have the username \"" + name + "\""));
-                                return 0;
-                            }
+                if (original == null || original.equals(player.getName())) {
+                    player.sendMessage(
+                            Component.text("You already have your default username"));
+                    return 0;
+                }
 
-                            if (millisDifference <= 2000) {
-                                throw EX_RATELIMIT.create();
-                            }
+                setUsername(player, original);
+                originalNames.remove(player);
+                return 1;
+            }));
+        }
 
-                            for (Player other : Bukkit.getOnlinePlayers()) {
-                                if (!other.getName().equalsIgnoreCase(name)) continue;
-                                throw EX_TAKEN.create();
-                            }
+        builder.then(argument("username", greedyString())
+                .executes(ctx -> {
+                    Player player = player(ctx);
 
-                            // Preserve UUIDs, as changing them breaks clients
-                            PlayerProfile newProfile =
-                                    Bukkit.createProfileExact(player.getUniqueId(), name);
-
-                            player.setPlayerProfile(newProfile);
-                            lastUsedMillis.put(player, System.currentTimeMillis());
-
-                            player.sendMessage(
-                                    Component.text("Successfully set your username to \"")
-                                            .append(Component.text(name))
-                                            .append(Component.text("\""))
+                    String nameColor =
+                            Utility.translateLegacyColors(
+                                    StringArgumentType.getString(
+                                            ctx,
+                                            "username"
+                                    )
                             );
-                            return 1;
-                        }));
+                    String name = nameColor.substring(0, Math.min(16,
+                            nameColor.length()));
+
+                    if (name.equals(player.getName())) {
+                        player.sendMessage(Component
+                                .text("You already have the username \"" + name + "\""));
+                        return 0;
+                    }
+
+                    originalNames.putIfAbsent(player, player.getName());
+                    setUsername(player, name);
+                    return 1;
+                }));
+    }
+
+    private void setUsername(Player player, String name)
+            throws CommandSyntaxException {
+        long millis = lastUsedMillis.getOrDefault(player, 0L);
+        long millisDifference = System.currentTimeMillis() - millis;
+
+        if (millisDifference <= RATELIMIT_MILLIS) {
+            throw EX_RATELIMIT.create();
+        }
+
+        for (Player other : Bukkit.getOnlinePlayers()) {
+            if (other.equals(player) || !other.getName().equalsIgnoreCase(name)) {
+                continue;
+            }
+            throw EX_TAKEN.create();
+        }
+
+        // Preserve UUIDs, as changing them breaks clients
+        PlayerProfile newProfile = Bukkit.createProfileExact(player.getUniqueId(), name);
+
+        player.setPlayerProfile(newProfile);
+        lastUsedMillis.put(player, System.currentTimeMillis());
+
+        player.sendMessage(
+                Component.text("Successfully set your username to \"")
+                        .append(Component.text(name))
+                        .append(Component.text("\""))
+        );
+    }
+
+    private static Player player(CommandContext<CommandSourceStack> ctx)
+            throws CommandSyntaxException {
+        if (ctx.getSource().getSender() instanceof Player player) {
+            return player;
+        }
+        throw EX_NOT_PLAYER.create();
     }
 }
